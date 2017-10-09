e292f13298858f9054ac9841a08faebf9e84f70f
mahuaibo <mahuaibo@360baige.com>
2017-10-09 17:41:28 +0800

    ma

--- src/components/application/AppTplDetail.vue
+++ src/components/application/AppTplDetail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="index">
+  <div class="application">
     <div class="appTplDetail">
       <div class="appTplDetail-left">
         <img :src="appTplData.image" class="application-image">
@@ -63,13 +63,15 @@
     <div class="appTplDetail-bottom">
       <div class="appTplDetail-bottom-payWay">支付方式</div>
       <div class="appTplDetail-bottom-img">
-        <div v-model="payMode" id="appTplDetail-bottom-img-weChatPay" @click="optionPayMode('weChatPay')">
-          <img src="../../assets/weixin.png" class="appTplDetail-bottom-imgs">
-          <label class="appTplDetail-bottom-img-weChatText">微信</label>
+        <div class="appTplDetail-bottom-img-item selected" id="appTplDetail-bottom-img-weChatPay"
+             @click="optionPayMode('weChatPay')">
+          <img src="../../assets/weixin.png" class="appTplDetail-bottom-img-item-logo">
+          <label class="appTplDetail-bottom-img-item-text">微&nbsp;&nbsp;信</label>
         </div>
-        <div v-model="payMode" id="appTplDetail-bottom-img-aliPay" @click="optionPayMode('aliPay')">
-          <img src="../../assets/zhifubao.png" class="appTplDetail-bottom-imgs">
-          <label class="appTplDetail-bottom-img-aliText">支付宝</label>
+        <div class="appTplDetail-bottom-img-item" id="appTplDetail-bottom-img-aliPay"
+             @click="optionPayMode('aliPay')">
+          <img src="../../assets/zhifubao.png" class="appTplDetail-bottom-img-item-logo">
+          <label class="appTplDetail-bottom-img-item-text">支付宝</label>
         </div>
       </div>
     </div>
@@ -85,7 +87,7 @@
 </template>
 <script>
   import axios from 'axios'
-  import {mapGetters, mapActions} from 'vuex'
+  import { mapGetters, mapActions } from 'vuex'
 
   export default {
     created () {
@@ -242,58 +244,92 @@
         } else {
           if (current.codeUrl === '') {
             console.log('1，' + current.codeUrl)
-//            current.payDialog = true
-//            current.payImage = current.publicParameters.domain + '/order/qr?size=356&url=' + current.codeUrl
             window.location.href = current.codeUrl + '&redirect_url=http://wwww.360baige.com'
-//            window.location.open(current.codeUrl)
             current.payStatusTimer(current.orderId)
           } else {
             console.log('2，' + current.codeUrl)
-//            current.payDialog = true
-//            current.payImage = current.publicParameters.domain + '/order/qr?size=356&url=' + current.codeUrl
             window.location.href = current.codeUrl + '&redirect_url=http://wwww.360baige.com'
-//            window.location.open(current.codeUrl)
             current.payStatusTimer(current.orderId)
           }
         }
       },
       // 立即支付
       immediatePayment () {
-        console.log('确认订单')
+        var selected = document.getElementsByClassName('appTplDetail-bottom-img-item selected')[0].id
         var current = this
-        if (current.orderId === 0) {
-          axios({
-            method: 'POST',
-            url: this.publicParameters.domain + '/order/add',
-            params: {
-              accessToken: localStorage.getItem('accessToken'),
-              applicationTplId: this.appTplData.id,
-              num: this.number,
-              payType: this.payMode
-            }
-          }).then(function (response) {
-            console.log(response.data)
-            if (response.data.code === '200') {
-              current.payDialog = true
-              current.codeUrl = response.data.data.codeUrl
-              current.payImage = current.publicParameters.domain + '/order/qr?size=356&url=' + current.codeUrl
-              current.payStatusTimer(response.data.data.id)
-              current.orderId = response.data.data.id
-            }
-          }).catch(function (error) {
-            console.log(error)
-          })
-        } else {
-          if (current.codeUrl === '') {
-            console.log('1，' + current.codeUrl)
-            current.payDialog = true
-            current.payImage = current.publicParameters.domain + '/order/qr?size=356&url=' + current.codeUrl
-            current.payStatusTimer(current.orderId)
+        if (selected === 'appTplDetail-bottom-img-aliPay') {
+          console.log('aliPay')
+          if (current.orderId === 0) {
+            axios({
+              method: 'POST',
+              url: this.publicParameters.domain + '/order/add',
+              params: {
+                accessToken: localStorage.getItem('accessToken'),
+                applicationTplId: this.appTplData.id,
+                num: this.number,
+                payType: '2'
+              }
+            }).then(function (response) {
+              console.log(response.data)
+              if (response.data.code === '200') {
+                current.payDialog = true
+                current.codeUrl = response.data.data.codeUrl
+                current.payImage = current.publicParameters.domain + '/order/qr?size=356&url=' + current.codeUrl
+                current.payStatusTimer(response.data.data.id)
+                current.orderId = response.data.data.id
+              }
+            }).catch(function (error) {
+              console.log(error)
+            })
           } else {
-            console.log('2，' + current.codeUrl)
-            current.payDialog = true
-            current.payImage = current.publicParameters.domain + '/order/qr?size=356&url=' + current.codeUrl
-            current.payStatusTimer(current.orderId)
+            if (current.codeUrl === '') {
+              console.log('1，' + current.codeUrl)
+              current.payDialog = true
+              current.payImage = current.publicParameters.domain + '/order/qr?size=356&url=' + current.codeUrl
+              current.payStatusTimer(current.orderId)
+            } else {
+              console.log('2，' + current.codeUrl)
+              current.payDialog = true
+              current.payImage = current.publicParameters.domain + '/order/qr?size=356&url=' + current.codeUrl
+              current.payStatusTimer(current.orderId)
+            }
+          }
+        } else if (selected === 'appTplDetail-bottom-img-weChatPay') {
+          console.log('weChatPay')
+          if (current.orderId === 0) {
+            axios({
+              method: 'POST',
+              url: this.publicParameters.domain + '/order/add',
+              params: {
+                accessToken: localStorage.getItem('accessToken'),
+                applicationTplId: this.appTplData.id,
+                num: this.number,
+                payType: '1'
+              }
+            }).then(function (response) {
+              console.log(response.data)
+              if (response.data.code === '200') {
+                current.payDialog = true
+                current.codeUrl = response.data.data.codeUrl
+                current.payImage = current.publicParameters.domain + '/order/qr?size=356&url=' + current.codeUrl
+                current.payStatusTimer(response.data.data.id)
+                current.orderId = response.data.data.id
+              }
+            }).catch(function (error) {
+              console.log(error)
+            })
+          } else {
+            if (current.codeUrl === '') {
+              console.log('1，' + current.codeUrl)
+              current.payDialog = true
+              current.payImage = current.publicParameters.domain + '/order/qr?size=356&url=' + current.codeUrl
+              current.payStatusTimer(current.orderId)
+            } else {
+              console.log('2，' + current.codeUrl)
+              current.payDialog = true
+              current.payImage = current.publicParameters.domain + '/order/qr?size=356&url=' + current.codeUrl
+              current.payStatusTimer(current.orderId)
+            }
           }
         }
       },
@@ -339,14 +375,11 @@
       // 切换支付方式
       optionPayMode (index) {
         if (index === 'weChatPay') {
-          document.getElementById(index).style.border = '1px solid #ff5f27'
-          document.getElementById('appTplDetail-bottom-img-aliPay').style.border = '1px solid #f0f0f0'
-          document.getElementById('appTplDetail-bottom-img-aliPay').style.borderLeft = '0px solid #ffffff'
+          document.getElementById('appTplDetail-bottom-img-aliPay').className = 'appTplDetail-bottom-img-item'
+          document.getElementById('appTplDetail-bottom-img-weChatPay').className = 'appTplDetail-bottom-img-item selected'
         } else {
-          this.messageRemind('warning', '猿类努力开发中...')
-//          document.getElementById(index).style.border = '1px solid #ff5f27'
-//          document.getElementById('appTplDetail-bottom-img-weChatPay').style.border = '1px solid #f0f0f0'
-//          document.getElementById('appTplDetail-bottom-img-weChatPay').style.borderRadius = '0px solid #ffffff'
+          document.getElementById('appTplDetail-bottom-img-aliPay').className = 'appTplDetail-bottom-img-item selected'
+          document.getElementById('appTplDetail-bottom-img-weChatPay').className = 'appTplDetail-bottom-img-item'
         }
       },
       messageRemind (type, info) { // type success成功   warning警告   error失败
@@ -357,205 +390,188 @@
   }
 </script>
 <style lang="scss" scoped>
-  .el-radio__label {
-    padding-left: 24px;
-  }
-
-  .el-dialog {
-    width: 300px;
-    .el-dialog__body {
-      padding: 0px 0px 10px 0px;
-    }
-  }
-
-  .index {
+  .application {
     min-width: 830px;
-    margin-left: 20px;
-    margin-right: 20px;
-  }
-
-  .appTplDetail {
-    font-size: 14px;
-    min-height: 118px;
-    border-bottom: 1px solid #f0f0f0;
-    .appTplDetail-left {
-      width: 70px;
-      height: 106px;
-      float: left;
-      .application-image {
+    margin: 0 20px;
+    .appTplDetail {
+      font-size: 14px;
+      min-height: 118px;
+      border-bottom: 1px solid #f0f0f0;
+      .appTplDetail-left {
         width: 70px;
-        height: 70px;
-        border-radius: 20px;
-      }
-      .application-status {
-        color: #ffffff;
-        font-size: 12px;
-        border: 1px solid #20a0ff;
-        border-radius: 3px;
-        background-color: #20a0ff;
-        margin-top: 6px;
-        width: 68px;
-        height: 22px;
-        line-height: 22px;
-      }
-    }
-    .appTplDetail-right {
-      margin-top: 12px;
-      padding-left: 132px;
-      text-align: left;
-      .appTplDetail-right-name {
-        color: #505050;
-        font-weight: bold;
-      }
-      #appTplDetail-right-descMore {
-        float: right;
-      }
-      #appTplDetail-right-desc {
-        padding-top: 6px;
-        height: 24px;
-        width: 550px;
-        overflow: hidden
-      }
-      #packUp {
-        display: none;
-        float: right;
-      }
-      .appTplDetail-right-feesDesc {
-        color: #505050;
-        font-weight: bold;
-        padding-top: 12px;
-        .appTplDetail-right-feesDesc-endTime {
+        height: 106px;
+        float: left;
+        .application-image {
+          width: 70px;
+          height: 70px;
+          border-radius: 20px;
+        }
+        .application-status {
+          color: #ffffff;
           font-size: 12px;
-          font-weight: normal;
+          border: 1px solid #20a0ff;
+          border-radius: 3px;
+          background-color: #20a0ff;
+          margin-top: 6px;
+          width: 68px;
+          height: 22px;
+          line-height: 22px;
         }
       }
-      .appTplDetail-right-feesExplain {
-        padding-top: 6px;
-        padding-bottom: 24px;
-        .appTplDetail-right-feesExplain-price {
-          color: #ff5f27;
+      .appTplDetail-right {
+        margin-top: 12px;
+        padding-left: 132px;
+        text-align: left;
+        .appTplDetail-right-name {
+          color: #505050;
           font-weight: bold;
         }
-      }
-    }
-  }
-
-  .appTplDetail-middle {
-    font-size: 14px;
-    padding-top: 24px;
-    padding-bottom: 24px;
-    border-bottom: 1px solid #f0f0f0;
-    .appTplDetail-middle-data {
-      padding-top: 6px;
-      .appTplDetail-middle-data-product {
-        padding-left: 12px;
-        .appTplDetail-middle-data-product-data {
-          min-width: 222px;
-          max-width: 222px;
-          height: 102px;
-          line-height: 102px;
-          border: 1px solid #ff5f27;
-          background-color: #ff5f27;
-          font-size: 14px;
-          .appTplDetail-middle-data-product-price {
-            float: left;
-            width: 163px;
-            height: 100px;
-            font-size: 24px;
+        #appTplDetail-right-descMore {
+          float: right;
+        }
+        #appTplDetail-right-desc {
+          padding-top: 6px;
+          height: 24px;
+          width: 550px;
+          overflow: hidden
+        }
+        #packUp {
+          display: none;
+          float: right;
+        }
+        .appTplDetail-right-feesDesc {
+          color: #505050;
+          font-weight: bold;
+          padding-top: 12px;
+          .appTplDetail-right-feesDesc-endTime {
+            font-size: 12px;
+            font-weight: normal;
+          }
+        }
+        .appTplDetail-right-feesExplain {
+          padding-top: 6px;
+          padding-bottom: 24px;
+          .appTplDetail-right-feesExplain-price {
             color: #ff5f27;
-            background: #ffffff;
-          }
-          .appTplDetail-middle-data-product-card {
-            float: right;
-            background-color: #f0f0f0;
-            width: 57px;
-            height: 100px;
-            border-bottom-right-radius: 25px;
-          }
-          .appTplDetail-middle-data-product-text {
-            margin: 20px;
-            min-width: 20px;
-            min-height: 57px;
-            line-height: 25px;
+            font-weight: bold;
           }
         }
       }
-      .appTplDetail-middle-data-price {
-        height: 104px;
-        line-height: 104px;
-      }
-      .appTplDetail-middle-data-num {
-        height: 70px;
-        padding-top: 34px;
-      }
-      .appTplDetail-middle-data-totalPrice {
-        height: 104px;
-        line-height: 104px;
-        color: #ff5f27;
-      }
     }
-  }
-
-  .appTplDetail-bottom {
-    text-align: left;
-    padding-top: 24px;
-    .appTplDetail-bottom-payWay {
-      color: #505050;
-      font-weight: bold;
-    }
-    .appTplDetail-bottom-img {
-      width: 222px;
-      height: 42px;
-      margin-top: 24px;
-    }
-    #appTplDetail-bottom-img-weChatPay {
-      height: 42px;
-      line-height: 42px;
-      border: 1px solid #ff5f27;
-      display: inline-block;
-      width: 49%;
-      .appTplDetail-bottom-img-weChatText {
-        padding-left: 6px;
-        letter-spacing: 14px;
-      }
-    }
-    #appTplDetail-bottom-img-aliPay {
-      height: 42px;
-      line-height: 42px;
-      display: inline-block;
-      width: 49%;
-      border-top: 1px solid #f0f0f0;
-      border-right: 1px solid #f0f0f0;
+    .appTplDetail-middle {
+      font-size: 14px;
+      padding-top: 24px;
+      padding-bottom: 24px;
       border-bottom: 1px solid #f0f0f0;
-      float: right;
-      .appTplDetail-bottom-img-aliText {
-        padding-left: 6px;
+      .appTplDetail-middle-data {
+        padding-top: 6px;
+        .appTplDetail-middle-data-product {
+          padding-left: 12px;
+          .appTplDetail-middle-data-product-data {
+            min-width: 222px;
+            max-width: 222px;
+            height: 102px;
+            line-height: 102px;
+            border: 1px solid #ff5f27;
+            background-color: #ff5f27;
+            font-size: 14px;
+            .appTplDetail-middle-data-product-price {
+              float: left;
+              width: 163px;
+              height: 100px;
+              font-size: 24px;
+              color: #ff5f27;
+              background: #ffffff;
+            }
+            .appTplDetail-middle-data-product-card {
+              float: right;
+              background-color: #f0f0f0;
+              width: 57px;
+              height: 100px;
+              border-bottom-right-radius: 25px;
+            }
+            .appTplDetail-middle-data-product-text {
+              margin: 20px;
+              min-width: 20px;
+              min-height: 57px;
+              line-height: 25px;
+            }
+          }
+        }
+        .appTplDetail-middle-data-price {
+          height: 104px;
+          line-height: 104px;
+        }
+        .appTplDetail-middle-data-num {
+          height: 70px;
+          padding-top: 34px;
+        }
+        .appTplDetail-middle-data-totalPrice {
+          height: 104px;
+          line-height: 104px;
+          color: #ff5f27;
+        }
       }
     }
-    .appTplDetail-bottom-imgs {
-      width: 20px;
-      height: 20px;
-      margin-left: 16px;
-      vertical-align: middle;
-      margin-bottom: 3px;
+    .appTplDetail-bottom {
+      text-align: left;
+      padding-top: 24px;
+      .appTplDetail-bottom-payWay {
+        color: #505050;
+        font-weight: bold;
+      }
+      .appTplDetail-bottom-img {
+        width: 242px;
+        height: 42px;
+        margin-top: 24px;
+        .appTplDetail-bottom-img-item {
+          width: 110px;
+          border: 1px solid #f0f0f0;
+          height: 42px;
+          float: left;
+          line-height: 42px;
+          display: inline-block;
+          .appTplDetail-bottom-img-item-logo {
+            width: 20px;
+            height: 20px;
+            margin-left: 16px;
+            vertical-align: middle;
+            margin-bottom: 3px;
+          }
+          .appTplDetail-bottom-img-item-text {
+            width: 100%;
+            padding: 6px;
+            font-size: 12px;
+            letter-spacing: 4px;
+          }
+        }
+        .appTplDetail-bottom-img-item.selected {
+          border: 1px solid #ff5f27;
+        }
+      }
+    }
+    .appTplDetail-immediatePayment {
+      float: right;
+      text-align: right;
+      width: 100%;
+      .confirm-order {
+        margin-top: 6px;
+        width: 107px;
+        height: 30px;
+        padding: 6px 15px 7px 15px;
+      }
+    }
+    .el-radio__label {
+      padding-left: 24px;
+    }
+    .el-dialog {
+      width: 300px;
+      .el-dialog__body {
+        padding: 0px 0px 10px 0px;
+      }
+      .pay-prompting {
+        padding-bottom: 40px;
+      }
     }
   }
-
-  .appTplDetail-immediatePayment {
-    float: right;
-    text-align: right;
-    width: 100%
-  }
-
-  .pay-prompting {
-    padding-bottom: 40px;
-  }
-
-  .confirm-order {
-    margin-top: 6px;
-    width: 107px;
-    height: 30px;
-    padding: 6px 15px 7px 15px;
-  }
-
 </style>
